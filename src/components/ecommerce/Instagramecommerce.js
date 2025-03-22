fe1e3766dd3d1e45bb875c7d12da8dffed2497c2
FarukmAliyu <153976343+FarukmAliyu@users.noreply.github.com>
2025-03-22 23:28:04 +0100

    Update Instagramecommerce.js

--- src/components/ecommerce/Instagramecommerce.js
+++ src/components/ecommerce/Instagramecommerce.js
@@ -1,155 +1,114 @@
-<template>
-  <div class="row">
-    <div class="col-12 d-flex justify-content-end">
-      <button @click="loginIg" class="btn btn-primary link-account-btn">Link Instagram Account</button>
-    </div>
-    <div class="col-xxl-7 col-xxxl-6">
-      <Instahappen />
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Instahappen from "../../components/Dashboard/Ecommerce/instagramhappen";
+import ExpectedEarnings from "../../components/Dashboard/Ecommerce/ExpectedEarnings";
+import ExpectedEarnings2 from "../../components/Dashboard/Ecommerce/ExpectedEarnings2";
+import RevenueThisMonth from "../../components/Dashboard/Ecommerce/RevenueThisMonth";
+import RevenueTargetByCountry from "../../components/Dashboard/CRMSystem/RevenueTargetByCountry";
+import ProjectStats from "../../components/Dashboard/ProjectManagement/ProjectStats";
+import NewCustomersThisMonth from "../../components/Dashboard/Ecommerce/NewCustomersThisMonth";
+import NewCustomersThisMonth2 from "../../components/Dashboard/Ecommerce/NewCustomersThisMonth2";
+import NewCustomersThisMonth3 from "../../components/Dashboard/Ecommerce/NewCustomersThisMonth3";
+import StatsBoxes from "../../components/Dashboard/Ecommerce/StatsBoxes";
+import DataBoxes from "../../components/Dashboard/Ecommerce/StatsBoxes/data";
+import WeeklySales from "../../components/Dashboard/Ecommerce/WeeklySales";
+import ChatContent from "../../components/Chat/ChatContent";
+import "./InstaCommerce.css"; // Assuming styles are in a separate file
 
-      <div class="row">
-        <div class="col-lg-6 col-xxxl-6 col-md-6">
-          <ExpectedEarnings />
-        </div>
-        <div class="col-lg-6 col-xxxl-6 col-md-6">
-          <ExpectedEarnings2 />
-        </div>
-        <div class="col-xl-12 col-lg-6 col-xxxl-6 col-md-12">
-          <RevenueThisMonth />
-        </div>
-        <div class="col-xl-12 col-lg-6 col-xxxl-6 col-md-12">
-          <NewCustomersThisMonth />
-        </div>
-      </div>
-    </div>
-    <div class="col-xxl-5 col-xxxl-6">
-      <StatsBoxes />
+const InstaCommerce = () => {
+  const [accessToken, setAccessToken] = useState(null);
 
-      <WeeklySales />
-    </div>
-    <div class="d-flex gap-5">
-      <div class="">
-        <RevenueTargetByCountry />
-      </div>
-      <div class="">
-        <div class="d-flex flex-column gap-3">
-          <div class="d-flex justify-content-between">
-            <NewCustomersThisMonth2 />
-            <NewCustomersThisMonth3 />
-          </div>
-          <DataBoxes />
-        </div>
-      </div>
-    </div>
+  const loginIg = () => {
+    axios
+      .get("https://skyestudio-backend.onrender.com/auth/login-ig")
+      .then((response) => {
+        window.location.href = response.data.redirectURL;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    <div>
-      <ProjectStats />
-    </div>
-    <div>
-      <h4>Comment & Messages</h4>
-      <ChatContent />
-    </div>
-  </div>
-</template>
+  const getAccessToken = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    if (code) {
+      axios
+        .get(`https://skyestudio-backend.onrender.com/auth/callback?code=${code}`)
+        .then((response) => {
+          const { access_token, user } = response.data;
+          setAccessToken(access_token);
 
-<script>
-/* eslint-disable no-undef */
-import { defineComponent } from "vue";
-
-import Instahappen from "../../components/Dashboard/Ecommerce/instagramhappen.vue";
-
-import ExpectedEarnings from "../../components/Dashboard/Ecommerce/ExpectedEarnings.vue";
-
-import ExpectedEarnings2 from "../../components/Dashboard/Ecommerce/ExpectedEarnings2.vue";
-
-import RevenueThisMonth from "../../components/Dashboard/Ecommerce/RevenueThisMonth.vue";
-
-import RevenueTargetByCountry from "../../components/Dashboard/CRMSystem/RevenueTargetByCountry/index.vue";
-
-import ProjectStats from "../../components/Dashboard/ProjectManagement/ProjectStats/index.vue";
-
-import NewCustomersThisMonth from "../../components/Dashboard/Ecommerce/NewCustomersThisMonth.vue";
-
-import NewCustomersThisMonth2 from "../../components/Dashboard/Ecommerce/NewCustomersThisMonth2.vue";
-
-import NewCustomersThisMonth3 from "../../components/Dashboard/Ecommerce/NewCustomersThisMonth3.vue";
-
-import StatsBoxes from "../../components/Dashboard/Ecommerce/StatsBoxes/index.vue";
-
-import DataBoxes from "../../components/Dashboard/Ecommerce/StatsBoxes/data.vue";
-
-import WeeklySales from "../../components/Dashboard/Ecommerce/WeeklySales.vue";
-
-import ChatContent from "../../components/Chat/ChatContent.vue";
-
-import axios from 'axios'
-
-export default defineComponent({
-  name: "InstaCommerce",
-  components: {
-    Instahappen,
-    ExpectedEarnings,
-    ExpectedEarnings2,
-    RevenueThisMonth,
-    RevenueTargetByCountry,
-    NewCustomersThisMonth,
-    NewCustomersThisMonth2,
-    NewCustomersThisMonth3,
-    StatsBoxes,
-    WeeklySales,
-    ProjectStats,
-    ChatContent,
-    DataBoxes,
-  },
-  data() {
-    return {
-      showOnboarding: false,
-      accessToken: null,
-    };
-  },
-  methods: {
-    loginIg() {
-      axios.get('https://skyestudio-backend.onrender.com/auth/login-ig')
-        .then(response => {
-          window.location.href = response.data.redirectURL;
+          localStorage.setItem("igAccessToken", access_token);
+          localStorage.setItem("igUserId", user.id);
+          localStorage.setItem("IgUsername", user.username);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
-    },
+    }
+  };
 
-    getAccessToken() {
-      const url = new URL(window.location.href);
-      const code = url.searchParams.get('code');
-      if (code) {
-        axios.get(`https://skyestudio-backend.onrender.com/auth/callback?code=${code}`)
-          .then(response => {
-            const accessToken = response.data.access_token;
-            const userId = response.data.user.id;
-            const userName = response.data.user.username;
-            this.accessToken = accessToken;
-            this.userId = userId;
-            this.userName = userName;
-            // Save access token and user id in local storage
-            localStorage.setItem('igAccessToken', accessToken);
-            localStorage.setItem('igUserId', userId);
-            localStorage.setItem('IgUsername', userName);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
-    },
-  },
+  useEffect(() => {
+    getAccessToken();
+  }, []);
 
-  mounted() {
-    this.getAccessToken();
-   }
-});
-</script>
-<style scoped>
-/* Button styling */
-.link-account-btn {
-  margin-bottom: 20px;
-  margin-left: 20px;
-}
-</style>
+  return (
+    <div className="row">
+      <div className="col-12 d-flex justify-content-end">
+        <button onClick={loginIg} className="btn btn-primary link-account-btn">
+          Link Instagram Account
+        </button>
+      </div>
+
+      <div className="col-xxl-7 col-xxxl-6">
+        <Instahappen />
+        <div className="row">
+          <div className="col-lg-6 col-xxxl-6 col-md-6">
+            <ExpectedEarnings />
+          </div>
+          <div className="col-lg-6 col-xxxl-6 col-md-6">
+            <ExpectedEarnings2 />
+          </div>
+          <div className="col-xl-12 col-lg-6 col-xxxl-6 col-md-12">
+            <RevenueThisMonth />
+          </div>
+          <div className="col-xl-12 col-lg-6 col-xxxl-6 col-md-12">
+            <NewCustomersThisMonth />
+          </div>
+        </div>
+      </div>
+
+      <div className="col-xxl-5 col-xxxl-6">
+        <StatsBoxes />
+        <WeeklySales />
+      </div>
+
+      <div className="d-flex gap-5">
+        <div>
+          <RevenueTargetByCountry />
+        </div>
+        <div>
+          <div className="d-flex flex-column gap-3">
+            <div className="d-flex justify-content-between">
+              <NewCustomersThisMonth2 />
+              <NewCustomersThisMonth3 />
+            </div>
+            <DataBoxes />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <ProjectStats />
+      </div>
+
+      <div>
+        <h4>Comment & Messages</h4>
+        <ChatContent />
+      </div>
+    </div>
+  );
+};
+
+export default InstaCommerce;
